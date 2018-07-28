1763a7fc390c444fa3802b8dec384aa03a114130
Cleber Junior <dwcleb@gmail.com>
2018-07-28 09:56:55 -0300

    Model and Migration User

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'root',
   password: null,
-  database: 'snipefty',
-  host: 'localhost',
+  database: 'snipetfy',
+  host: '127.0.0.1',
   dialect: 'mysql',
 };
--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const app = express();
 
+const { User } = require('./app/models');
+
+User.create({ name: 'Cleber', email: 'dwcleb@gmail.com', password: 'cleber' });
+
 nunjuckus.configure(path.resolve('app', 'views'), {
   autoescape: true,
   express: app,
