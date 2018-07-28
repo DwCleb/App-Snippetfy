374d30e0cba118f12534a8b8d59073800d199de3
Cleber Junior <dwcleb@gmail.com>
2018-07-28 13:26:11 -0300

    MVC architecture

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nunjuckus = require('nunjucks');
 const path = require('path');
+const routes = require('./app/routes');
 
 const app = express();
-
-const { User } = require('./app/models');
-
-User.create({ name: 'Cleber', email: 'dwcleb@gmail.com', password: 'cleber' });
 
 nunjuckus.configure(path.resolve('app', 'views'), {
   autoescape: true,
@@ -17,8 +14,6 @@ nunjuckus.configure(path.resolve('app', 'views'), {
 app.set('view engine', 'njk');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+app.use('/', routes);
 
 app.listen(3000);
